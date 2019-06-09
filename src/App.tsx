f051fcaf2320b4e83a50b8d7b31b02573b4e4e35
Tsukasa Murashige <tsukasamurashige@gmail.com>
2019-06-09 16:24:02 +0900

    Sort by gender

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,45 @@ import axios from 'axios'
 import styled from 'styled-components'
 
 import UserList from './components/UserList'
+import { GENDERS } from './const/sortOptions'
+
+interface IGender {
+  value: string,
+  name: string
+}
 
 const App: React.FC = () => {
   const [users, setUsers] = useState([])
-
-  console.log('users', users)
+  const [queries, setQeries] = useState({
+    results: 100,
+    gender: ''
+  })
 
   useEffect(() => {
     const getUsers = async () => {
-      const result = await axios.get("https://randomuser.me/api/?results=100")
+      const result = await axios.get('https://randomuser.me/api/', {
+        params: queries
+      })
       setUsers(result.data.results)
     }
     
     getUsers()
-  }, [])
+  }, [queries])
+
+  console.log('users', users, 'queries', queries)
+
+
+
+  const onChangeGender = (gender: string) => {
+    setQeries({
+      ...queries,
+      gender
+    })
+  }
+
+  const renderGenderOptions = (genders: Array<IGender>) => {
+    return genders.map((gender) => <option value={ gender.value } key={ gender.value }>{ gender.name }</option>)
+  }
 
   const renderUserList = (users: Array<object>) => {
     const isLoaded = users.length > 0
@@ -30,6 +55,12 @@ const App: React.FC = () => {
 
   return (
     <StyledContainer>
+      <StyledSortContent>
+        <label>性別で絞り込み：</label>
+        <select onChange={ (e) => onChangeGender(e.target.value) }>
+          { renderGenderOptions(GENDERS) }
+        </select>
+      </StyledSortContent>
       { renderUserList(users) }
     </StyledContainer>
   )
@@ -38,6 +69,13 @@ const App: React.FC = () => {
 const StyledContainer = styled.div`
   width: 100%;
   padding: 15px;
+`
+
+const StyledSortContent = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 25px;
+  padding: 0 15px;
 `
 
 export default App
